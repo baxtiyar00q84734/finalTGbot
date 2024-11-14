bc2d36e8e8333f05197a643d32e51977e923d56d
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-11-14 15:53:01 +0400

    project init

---
+++ final-tg-bot/src/main/java/org/example/finaltgbot/FinalTgBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.finaltgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalTgBotApplication.class, args);
+    }
+
+}
